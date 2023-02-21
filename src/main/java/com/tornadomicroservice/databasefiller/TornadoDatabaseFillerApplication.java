0d880d208e8744eebead53e2ee9a761f5bb3d2c3
Gaetano Di Grazia <107346930+gaetanodigrazia@users.noreply.github.com>
2023-02-22 00:43:00 +0100

    Initial commit

---
+++ src/main/java/com/tornadomicroservice/databasefiller/TornadoDatabaseFillerApplication.java
@@ -0,0 +1,13 @@
+package com.tornadomicroservice.databasefiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornadoDatabaseFillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornadoDatabaseFillerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tornadomicroservice/databasefiller/TornadoDatabaseFillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornadomicroservice.databasefiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornadoDatabaseFillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
